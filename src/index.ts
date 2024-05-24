09febaa1fe21b94469a38d2282c53ba6118be6f2
sushantsharmaa <sushant.x01@gmail.com>
2024-05-24 13:53:47 +0530

    express server setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log('Starting...');
+import express, { Request, Response } from 'express';
+
+const app = express();
+
+app.get('/', (req: Request, res: Response) => {
+  res.send(`
+    <div>
+     <h1>Hie there..!</h1>
+    </div>
+  `);
+});
+
+app.listen(3000, () => {
+  console.log('listening on port 3000');
+});
